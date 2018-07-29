b579841844acaae6543dda7ff77af49f3c243f7a
donglei <dl580231@163.com>
2018-07-30 02:59:59 +0800

    add entity for area and personInfo

---
+++ src/main/java/com/nuc/o2o/entity/Area.java
@@ -0,0 +1,47 @@
+package com.nuc.o2o.entity;
+
+import java.util.Date;
+
+public class Area {
+	//ID
+	private Integer area_id;
+	//名称
+	private String areaName;
+	//权重（优先级）
+	private Integer priority;
+	//创建时间
+	private Date createTime;
+	//更新时间
+	private Date lastEditTime;
+	public Integer getArea_id() {
+		return area_id;
+	}
+	public void setArea_id(Integer area_id) {
+		this.area_id = area_id;
+	}
+	public String getAreaName() {
+		return areaName;
+	}
+	public void setAreaName(String areaName) {
+		this.areaName = areaName;
+	}
+	public Integer getPriority() {
+		return priority;
+	}
+	public void setPriority(Integer priority) {
+		this.priority = priority;
+	}
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+	public Date getLastEditTime() {
+		return lastEditTime;
+	}
+	public void setLastEditTime(Date lastEditTime) {
+		this.lastEditTime = lastEditTime;
+	}
+	
+}
